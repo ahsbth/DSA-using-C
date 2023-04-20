8dfbbd29a72a21a0d578ce74d5da1834810f0306
ahsbth <asraj48342@gmail.com>
2023-04-20 16:00:30 +0530

    matrix

---
+++ Matrix.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int mat[2][3],i,j;
+    printf("Enter the elements of matrix=");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",mat[i][j]);
+        }
+    }
+    printf("\nMatrix\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t",mat[i][j]);
+        }
+        printf("\n");
+    }
+}
