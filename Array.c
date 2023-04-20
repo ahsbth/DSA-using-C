5820f39fbdc324fdf1edce6c8f7d8f29890d78a8
ahsbth <asraj48342@gmail.com>
2023-04-20 15:39:34 +0530

    searhed no

--- Array.c
+++ Array.c
@@ -1,1 +1,25 @@
-kjsskjnwfn
+#include<stdio.h>
+void main()
+{
+    int a[5],i,n,p=0,pos;
+    printf("Enter the elemnts in Array=");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("ENter the no that you want to search=");
+    scanf("%d",&n);
+    for(i=0;i<5;i++)
+    {
+        if(n==a[i])
+        {
+            p=1;
+            break;
+            pos=i+1
+        }
+    }
+    if(p==1)
+    {
+        printf(" %d is found at position=%d",n,pos);
+    }
+}
